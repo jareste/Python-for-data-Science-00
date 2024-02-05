004616c6d057ca8b7fb374647dfa9498bd233f74
Joan Areste Gonzalez <jareste2000@gmail.com>
2024-02-05 13:52:48 +0100

    ex09 done

---
+++ ex09/ft_package/README.md
@@ -0,0 +1,11 @@
+To build the library execute:
+
+    - python3 setup.py sdist bdist_wheel
+
+To add it use:
+    - python3 -m pip install ./dist/ft_package-0.0.1.tar.gz
+    or
+    - python3 -m pip install ./dist/ft_package-0.0.1-py3-none-any.whl
+
+To check the package:
+    - python3 -m pip show -v ft_package
---
+++ ex09/ft_package/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ft_package",
+    version="0.0.1",
+    url="https://github.com/eagle/ft_package",
+    author="eagle",
+    author_email="eagle@42.fr",
+    description="A sample test package",
+    packages=find_packages(),
+    install_requires=[],
+    license="MIT",
+    license_file="LICENSE",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+)
